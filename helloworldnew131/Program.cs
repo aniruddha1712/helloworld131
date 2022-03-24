3ffa8642e4e4c84ec25c1e63f839c2ecc4fd944d
ANIRUDDHA MISHRA <100330262+aniruddha1712@users.noreply.github.com>
2022-03-24 14:16:05 +0530

    Update Program.cs

--- helloworldnew131/Program.cs
+++ helloworldnew131/Program.cs
@@ -12,17 +12,9 @@ namespace helloworldnew131
         {
             Console.WriteLine("Welcome to 131");
 
-<<<<<<< HEAD
             Reversenumber.FindReverse();
 
             Swapnumber.SwapTwoNumbers();
-=======
-
-            Reversenumber.FindReverse();
-          
-            Swapnumber.SwapTwoNumbers();
-
->>>>>>> f8d2ebe2cb81f419d9b1c28b3935a289d39371ef
 
             Console.ReadLine();
 
